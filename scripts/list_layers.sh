cfec62716eb094af546504ebca3d0f6b47df7bd4
Stephen Pinkerton <stephen.pinkerton@datadoghq.com>
2019-05-03 13:38:30 -0400

    Add copyright header to source files

--- scripts/list_layers.sh
+++ scripts/list_layers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
+
 # Lists most recent layers ARNs across regions to STDOUT
 # Optionals args: [layer-name] [region]
 
