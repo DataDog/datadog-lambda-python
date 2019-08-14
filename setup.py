0b3894af121b4ef92b008b132ac2e57c734872fa
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2019-08-14 17:46:29 -0400

    Add protocol buffer dependency

--- setup.py
+++ setup.py
@@ -6,40 +6,41 @@ from datadog_lambda import __version__
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='datadog_lambda',
+    name="datadog_lambda",
     version=__version__,
-    description='The Datadog AWS Lambda Layer',
+    description="The Datadog AWS Lambda Layer",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/DataDog/datadog-lambda-layer-python',
-    author='Datadog, Inc.',
-    author_email='dev@datadoghq.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/DataDog/datadog-lambda-layer-python",
+    author="Datadog, Inc.",
+    author_email="dev@datadoghq.com",
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='datadog aws lambda layer',
-    packages=['datadog_lambda'],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
+    keywords="datadog aws lambda layer",
+    packages=["datadog_lambda", "datadog_lambda.pb"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4",
     install_requires=[
-        'aws-xray-sdk==2.4.2',
-        'datadog==0.28.0',
-        'wrapt==1.11.1',
-        'setuptools==40.8.0',
-        'boto3==1.9.160',
-        'grpcio==1.22.0'
+        "aws-xray-sdk==2.4.2",
+        "datadog==0.28.0",
+        "wrapt==1.11.1",
+        "setuptools==40.8.0",
+        "boto3==1.9.160",
+        "grpcio==1.22.0",
+        "protobuf==3.9.1",
     ],
     extras_require={
-        'dev': [
-            'nose2==0.9.1',
-            'flake8==3.7.8',
-            'requests==2.21.0',
-            'grpcio-tools==1.22.0'
+        "dev": [
+            "nose2==0.9.1",
+            "flake8==3.7.8",
+            "requests==2.21.0",
+            "grpcio-tools==1.22.0",
         ]
-    }
+    },
 )
