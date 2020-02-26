b3ebc80b0bf7111a09e693e197ce8c950093c79f
Stephen Firrincieli <stephen.firrincieli@datadoghq.com>
2020-02-26 17:35:57 -0500

    Make integration test work for async metrics

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Stop execution if any command has errors
 set -e
 
 # These values need to be in sync with serverless.yml, where there needs to be a function
@@ -16,10 +17,16 @@ integration_tests_dir="$repo_dir/tests/integration"
 
 script_start_time=$(date --iso-8601=seconds)
 
+mismatch_found=false
+
 echo "Start time is $script_start_time"
 
-echo "Building new layers that will be uploaded with our test functions"
+echo "Building layers that will be deployed with our test functions"
 # source $scripts_dir/build_layers.sh
+
+if [ -n "$OVERWRITE" ]; then
+    echo "Overwriting snapshots in this execution"
+fi
 
 echo "Deploying functions"
 cd $integration_tests_dir
@@ -28,25 +35,72 @@ serverless deploy
 echo "Invoking functions"
 for handler_name in "${LAMBDA_HANDLERS[@]}"; do
     for runtime in "${RUNTIMES[@]}"; do
-        echo "Invoking $handler_name-$runtime"
-        serverless invoke -f "$handler_name-$runtime"
+        function_name="$handler_name-$runtime"
+        function_snapshot_path="./snapshots/$function_name.return_value"
+
+        return_value=$(serverless invoke -f $function_name)
+
+        if [ -n "$OVERWRITE" ]; then
+            # If $OVERWRITE is set to true, write the new logs over the current snapshot
+            echo "Overwriting return value snapshot for $function_name"
+            echo "$return_value" >$function_snapshot_path
+        else
+            # Compare new return value to snapshot
+            set +e # Don't exit this script if there is a diff
+            diff_output=$(echo "$return_value" | diff - $function_snapshot_path)
+            if [ $? -eq 1 ]; then
+                echo "FAILURE: Return value for $function_name does not match snapshot:"
+                echo "$diff_output"
+                mismatch_found=true
+            else
+                echo "SUCCESS: Return value for $function_name matches snapshot"
+            fi
+            set -e
+        fi
     done
 done
 
-echo "Sleeping for $LOGS_WAIT_SECONDS seconds to wait for logs to appear in CloudWatch..."
+echo "Sleeping $LOGS_WAIT_SECONDS seconds to wait for logs to appear in CloudWatch..."
 sleep $LOGS_WAIT_SECONDS
 
 echo "Fetching logs for invocations and comparing to snapshots"
 for handler_name in "${LAMBDA_HANDLERS[@]}"; do
     for runtime in "${RUNTIMES[@]}"; do
-        logs=$(serverless logs -f "$handler_name-$runtime" --startTime $script_start_time)
-        python compare_to_snapshots.py "$handler_name-$runtime" "$logs"
+        function_name="$handler_name-$runtime"
+        function_snapshot_path="./snapshots/$function_name.logs"
+
+        # Fetch logs with serverless cli
+        logs=$(serverless logs -f $function_name --startTime $script_start_time)
+
+        # Filter serverless cli errors
+        logs=$(echo "$logs" | sed '/Serverless: Recoverable error occurred/d')
+
+        # Replace invocation-specific data with XXXX to normalize between executions
+        logs=$(echo "$logs" | sed -E 's/(RequestId|TraceId|SegmentId|Duration|Memory Used|"e"): [a-z0-9\.\-]+/\1: XXXX/g')
+
+        if [ -n "$OVERWRITE" ]; then
+            # If $OVERWRITE is set to true, write the new logs over the current snapshot
+            echo "Overwriting snapshot for $function_name"
+            echo "$logs" >$function_snapshot_path
+        else
+            # Compare new logs to snapshots
+            set +e # Don't exit this script if there is a diff
+            diff_output=$(echo "$logs" | diff - $function_snapshot_path)
+            if [ $? -eq 1 ]; then
+                echo "FAILURE: Mismatch found between new $function_name logs and snapshot:"
+                echo "$diff_output"
+                mismatch_found=true
+            else
+                echo "SUCCESS: New logs for $function_name match snapshot"
+            fi
+            set -e
+        fi
     done
 done
 
-# Go back to the repo root
-cd $repo_dir
+if [ "$mismatch_found" = true ]; then
+    echo "TEST FAILED: A mismatch between newly generated logs and a snapshot was found above. If this is expected, re-run this script with OVERWRITE=true to generate new snapshots"
+    exit 1
+fi
 
-# Download the new CloudWatch logs for each Lambda
-
-# Filter out all logs that aren't metrics and traces and compare to the snapshots in this repo
+echo "TEST SUCCEEDED: No difference found between new logs and snapshots"
