6b3dab3f4ab5c3b0c9e8885660202f065e7f2c69
Tian Chu <tian.chu@datadoghq.com>
2020-03-09 17:56:14 -0400

    Bump version to 2.14.0 (v14)

--- datadog_lambda/__init__.py
+++ datadog_lambda/__init__.py
@@ -1,6 +1,6 @@
 # The minor version corresponds to the Lambda layer version.
 # E.g.,, version 0.5.0 gets packaged into layer version 5.
-__version__ = "2.13.0"
+__version__ = "2.14.0"
 
 
 import os
