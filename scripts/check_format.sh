94b12fb6c0e3ef8a410dc9126626a190a10381c4
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-03-24 16:18:48 -0400

    Update check-format to be python3 compatible

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-PYTHON_VERSION=$(python -c 'import sys; print sys.version_info.major')
+PYTHON_VERSION=$(python -c 'import sys; print(sys.version_info.major)')
 if [ "$PYTHON_VERSION" = "2" ]; then
     echo "Skipping formatting, black not compatible with python 2"
     exit 0
