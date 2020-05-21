411eb12fc968a15f1e0bad5897a1edd961d10f9a
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-05-21 15:26:52 -0400

    Add copyright claim

--- datadog_lambda/handler.py
+++ datadog_lambda/handler.py
@@ -1,5 +1,11 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2020 Datadog, Inc.
+
 from __future__ import absolute_import
 from importlib import import_module
+
 import os
 from datadog_lambda.wrapper import datadog_lambda_wrapper
 
