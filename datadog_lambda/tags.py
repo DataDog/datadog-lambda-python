92a35b807abc75e801736cc127ff94f81c7d2dcd
Jordan Storms <jordan.storms@datadoghq.com>
2020-06-12 10:46:16 -0400

    update tag collection and tests

--- datadog_lambda/tags.py
+++ datadog_lambda/tags.py
@@ -25,29 +25,47 @@ def tag_dd_lambda_layer(tags):
         return [dd_lambda_layer_tag]
 
 
-def parse_lambda_tags_from_arn(arn):
+def parse_lambda_tags_from_arn(lambda_context):
     """Generate the list of lambda tags based on the data in the arn
     Args:
-        arn (str): Lambda ARN.
-            ex: arn:aws:lambda:us-east-1:123597598159:function:my-lambda[:optional-version]
+        lambda_context: Aws lambda context object
+            ex: lambda_context.arn = arn:aws:lambda:us-east-1:123597598159:function:my-lambda:1
     """
-    # Cap the number of times to split
-    split_arn = arn.split(":")
+    # Set up flag for extra testing to distinguish between a version or alias
+    hasAlias = False
+    # Cap the number of times to spli
+    split_arn = lambda_context.invoked_function_arn.split(":")
 
     if len(split_arn) > 7:
+        hasAlias = True
         _, _, _, region, account_id, _, function_name, alias = split_arn
-        resource = function_name + ":" + alias
-
     else:
         _, _, _, region, account_id, _, function_name = split_arn
-        resource = function_name
 
-    return [
+    # Add the standard tags to a list
+    tags = [
         "region:{}".format(region),
         "account_id:{}".format(account_id),
         "functionname:{}".format(function_name),
-        "resource:{}".format(resource),
     ]
+
+    # Check if we have a version or alias
+    if hasAlias:
+        # If $Latest, drop the $ for datadog tag convention. A lambda alias can't start with $
+        if alias.startswith("$"):
+            alias = alias[1:]
+        # Versions are numeric. Aliases need the executed version tag
+        elif not check_if_number(alias):
+            tags.append("executedversion:{}".format(lambda_context.function_version))
+        # create resource tag with function name and alias/version
+        resource = "resource:{}:{}".format(function_name, alias)
+    else:
+        # Resource is only the function name otherwise
+        resource = "resource:{}".format(function_name)
+
+    tags.append(resource)
+
+    return tags
 
 
 def get_runtime_tag():
@@ -63,10 +81,19 @@ def get_runtime_tag():
 def get_enhanced_metrics_tags(lambda_context):
     """Get the list of tags to apply to enhanced metrics
     """
-    return parse_lambda_tags_from_arn(lambda_context.invoked_function_arn) + [
+    return parse_lambda_tags_from_arn(lambda_context) + [
         get_cold_start_tag(),
         "memorysize:{}".format(lambda_context.memory_limit_in_mb),
-        "executedversion:{}".format(lambda_context.function_version),
         get_runtime_tag(),
         _format_dd_lambda_layer_tag(),
     ]
+
+
+def check_if_number(alias):
+    """ Check if the alias is a version or number. Python 2 has no easy way to test this like Python 3
+    """
+    try:
+        float(alias)
+        return True
+    except ValueError:
+        return False
--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,11 +1,21 @@
 import unittest
 
 try:
-    from unittest.mock import patch
+    from unittest.mock import patch, MagicMock
 except ImportError:
-    from mock import patch
+    from mock import patch, MagicMock
 
 from datadog_lambda.tags import parse_lambda_tags_from_arn, get_runtime_tag
+
+
+def get_mock_context(
+    invoked_function_arn="arn:aws:lambda:us-east-1:1234597598159:function:swf-hello-test:$Latest",
+    function_version="1",
+):
+    lambda_context = MagicMock()
+    lambda_context.invoked_function_arn = invoked_function_arn
+    lambda_context.function_version = function_version
+    return lambda_context
 
 
 class TestMetricTags(unittest.TestCase):
@@ -14,16 +24,45 @@ class TestMetricTags(unittest.TestCase):
         self.mock_python_version_tuple = patcher.start()
         self.addCleanup(patcher.stop)
 
-    def test_parse_lambda_tags_from_arn(self):
+    def test_parse_lambda_tags_from_arn_latest(self):
         self.assertListEqual(
-            parse_lambda_tags_from_arn(
-                "arn:aws:lambda:us-east-1:1234597598159:function:swf-hello-test:function-alias"
-            ),
+            parse_lambda_tags_from_arn(get_mock_context()),
             [
                 "region:us-east-1",
                 "account_id:1234597598159",
                 "functionname:swf-hello-test",
-                "resource:swf-hello-test:function-alias",
+                "resource:swf-hello-test:Latest",
+            ],
+        )
+
+    def test_parse_lambda_tags_from_arn_version(self):
+        lambda_context = get_mock_context()
+        lambda_context.invoked_function_arn = (
+            "arn:aws:lambda:us-east-1:1234597598159:function:swf-hello-test:3"
+        )
+        self.assertListEqual(
+            parse_lambda_tags_from_arn(lambda_context),
+            [
+                "region:us-east-1",
+                "account_id:1234597598159",
+                "functionname:swf-hello-test",
+                "resource:swf-hello-test:3",
+            ],
+        )
+
+    def test_parse_lambda_tags_from_arn_alias(self):
+        lambda_context = get_mock_context()
+        lambda_context.invoked_function_arn = (
+            "arn:aws:lambda:us-east-1:1234597598159:function:swf-hello-test:my_alias-1"
+        )
+        self.assertListEqual(
+            parse_lambda_tags_from_arn(lambda_context),
+            [
+                "region:us-east-1",
+                "account_id:1234597598159",
+                "functionname:swf-hello-test",
+                "executedversion:1",
+                "resource:swf-hello-test:my_alias-1",
             ],
         )
 
