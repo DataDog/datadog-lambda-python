e5e6d6b23c66ca02c7fbe936aafef169975ddd0c
Sergio Prada <chechopradaster@gmail.com>
2020-08-25 16:58:29 -0400

    format tests

--- tests/test_module_name.py
+++ tests/test_module_name.py
@@ -10,11 +10,12 @@ except ImportError:
 from datadog_lambda.module_name import modify_module_name
 
 
-
 class TestModifyModuleName(unittest.TestCase):
-
     def test_modify_module_name(self):
-        self.assertEqual(modify_module_name('lambda/handler/name.bar'), 'lambda.handler.name.bar')
-        self.assertEqual(modify_module_name('lambda.handler/name.biz'), 'lambda.handler.name.biz')
-        self.assertEqual(modify_module_name('foo.handler'), 'foo.handler')
-
+        self.assertEqual(
+            modify_module_name("lambda/handler/name.bar"), "lambda.handler.name.bar"
+        )
+        self.assertEqual(
+            modify_module_name("lambda.handler/name.biz"), "lambda.handler.name.biz"
+        )
+        self.assertEqual(modify_module_name("foo.handler"), "foo.handler")
