b2a9363ad228ef1b4fca17409349635bdb5999f7
Alex DelVecchio <alex@checkhq.com>
2020-08-26 20:03:08 -0400

    Update package version number to 2.20.0 to correspond with Release v20

--- datadog_lambda/__init__.py
+++ datadog_lambda/__init__.py
@@ -1,6 +1,6 @@
 # The minor version corresponds to the Lambda layer version.
 # E.g.,, version 0.5.0 gets packaged into layer version 5.
-__version__ = "2.19.0"
+__version__ = "2.20.0"
 
 
 import os
