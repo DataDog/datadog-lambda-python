c3300d147a6c5b7bb7d013253bb61c393a666c4b
Nicolas Hinsch <nick.hinsch@datadoghq.com>
2021-03-30 17:48:04 -0400

    Combine integration tests for metrics and tracing (#123)

--- tests/integration/handle.py
+++ tests/integration/handle.py
@@ -1,10 +1,12 @@
+import requests
+
 from datadog_lambda.metric import lambda_metric
 from datadog_lambda.wrapper import datadog_lambda_wrapper
 
 
 @datadog_lambda_wrapper
 def handle(event, context):
-    # Parse request ID and record ids out of the event to include in the response
+    # Parse request ID and record IDs out of the event to include in the response
     request_id = event.get("requestContext", {}).get("requestId")
     event_records = event.get("Records", [])
 
@@ -17,10 +19,15 @@ def handle(event, context):
         if record.get("Sns", {}).get("MessageId"):
             record_ids.append(record["Sns"]["MessageId"])
 
+    # Generate custom metrics
     lambda_metric("hello.dog", 1, tags=["team:serverless", "role:hello"])
     lambda_metric(
         "tests.integration.count", 21, tags=["test:integration", "role:hello"]
     )
+
+    # Make HTTP calls to test ddtrace instrumentation
+    requests.get("https://httpstat.us/200/")
+    requests.get("https://httpstat.us/400/")
 
     return {
         "statusCode": 200,
