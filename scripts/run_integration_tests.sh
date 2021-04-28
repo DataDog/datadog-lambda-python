922a9564ed2afd8cd2058f3c6b2ff1dde3bfd41e
Maxime David <got2bex@gmail.com>
2021-04-28 18:33:56 -0400

    Add the matrix strategy to workflow to make integration tests faster (#137)
    * integration tests run faster

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -24,6 +24,27 @@ script_utc_start_time=$(date -u +"%Y%m%dT%H%M%S")
 
 mismatch_found=false
 
+# Format :
+# [0]: serverless runtime name
+# [1]: python version
+# [2]: random 8-character ID to avoid collisions with other runs
+python27=("python2.7" "2.7" $(xxd -l 4 -c 4 -p < /dev/random))
+python36=("python3.6" "3.6" $(xxd -l 4 -c 4 -p < /dev/random))
+python37=("python3.7" "3.7" $(xxd -l 4 -c 4 -p < /dev/random))
+python38=("python3.8" "3.8" $(xxd -l 4 -c 4 -p < /dev/random))
+
+PARAMETERS_SETS=("python27" "python36" "python37" "python38")
+
+if [ -z "$RUNTIME_PARAM" ]; then
+    echo "Python version not specified, running for all python versions."
+else
+    RUNTIME_PARAM_NO_DOT=$(echo $RUNTIME_PARAM | sed 's/\.//')
+    echo "Python version is specified: $RUNTIME_PARAM"
+    PARAMETERS_SETS=(python${RUNTIME_PARAM_NO_DOT})
+    BUILD_LAYER_VERSION=python$RUNTIME_PARAM_NO_DOT[1]
+fi
+
+
 if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
     echo "No AWS credentials were found in the environment."
     echo "Note that only Datadog employees can run these integration tests."
@@ -41,7 +62,7 @@ fi
 
 if [ -n "$BUILD_LAYERS" ]; then
     echo "Building layers that will be deployed with our test functions"
-    source $scripts_dir/build_layers.sh
+    PYTHON_VERSION=${!BUILD_LAYER_VERSION} source $scripts_dir/build_layers.sh
 else
     echo "Not building layers, ensure they've already been built or re-run with 'BUILD_LAYERS=true DD_API_KEY=XXXX ./scripts/run_integration_tests.sh'"
 fi
@@ -52,32 +73,48 @@ input_event_files=$(ls ./input_events)
 # Sort event files by name so that snapshots stay consistent
 input_event_files=($(for file_name in ${input_event_files[@]}; do echo $file_name; done | sort))
 
-# Generate a random 8-character ID to avoid collisions with other runs
-run_id=$(xxd -l 4 -c 4 -p < /dev/random)
-
-# Always remove the stack before exiting, no matter what
+# Always remove the stack(s) before exiting, no matter what
 function remove_stack() {
-    echo "Removing functions"
-    serverless remove --stage $run_id
+    for parameters_set in "${PARAMETERS_SETS[@]}"; do
+        serverless_runtime=$parameters_set[0]
+        python_version=$parameters_set[1]
+        run_id=$parameters_set[2]
+        echo "Removing stack for stage : ${!run_id}"
+        PYTHON_VERSION=${!python_version} RUNTIME=$parameters_set SERVERLESS_RUNTIME=${!serverless_runtime} \
+        serverless remove --stage ${!run_id}
+    done
 }
+
+
+
 trap remove_stack EXIT
 
-echo "Deploying functions"
-serverless deploy --stage $run_id
+for parameters_set in "${PARAMETERS_SETS[@]}"; do
+    
+    serverless_runtime=$parameters_set[0]
+    python_version=$parameters_set[1]
+    run_id=$parameters_set[2]
 
-echo "Invoking functions"
-set +e # Don't exit this script if an invocation fails or there's a diff
-for handler_name in "${LAMBDA_HANDLERS[@]}"; do
-    for runtime in "${RUNTIMES[@]}"; do
-        function_name="${handler_name}_${runtime}"
+    echo "Deploying functions for runtime : $parameters_set, serverless runtime : ${!serverless_runtime}, \
+python version : ${!python_version} and run id : ${!run_id}"
 
+    PYTHON_VERSION=${!python_version} RUNTIME=$parameters_set SERVERLESS_RUNTIME=${!serverless_runtime} \
+    serverless deploy --stage ${!run_id}
+
+    echo "Invoking functions for runtime $parameters_set"
+    set +e # Don't exit this script if an invocation fails or there's a diff
+    for handler_name in "${LAMBDA_HANDLERS[@]}"; do
+        
+        function_name="${handler_name}_python"
+        echo "$function_name"
         # Invoke function once for each input event
         for input_event_file in "${input_event_files[@]}"; do
             # Get event name without trailing ".json" so we can build the snapshot file name
             input_event_name=$(echo "$input_event_file" | sed "s/.json//")
-            snapshot_path="./snapshots/return_values/${function_name}_${input_event_name}.json"
+            snapshot_path="./snapshots/return_values/${handler_name}_${parameters_set}_${input_event_name}.json"
 
-            return_value=$(serverless invoke -f $function_name --stage $run_id --path "./input_events/$input_event_file")
+            return_value=$(PYTHON_VERSION=${!python_version} RUNTIME=$parameters_set SERVERLESS_RUNTIME=${!serverless_runtime} \
+            serverless invoke --stage ${!run_id} -f "$function_name" --path "./input_events/$input_event_file")
 
             if [ ! -f $snapshot_path ]; then
                 # If the snapshot file doesn't exist yet, we create it
@@ -109,14 +146,17 @@ sleep $LOGS_WAIT_SECONDS
 set +e # Don't exit this script if there is a diff or the logs endpoint fails
 echo "Fetching logs for invocations and comparing to snapshots"
 for handler_name in "${LAMBDA_HANDLERS[@]}"; do
-    for runtime in "${RUNTIMES[@]}"; do
-        function_name="${handler_name}_${runtime}"
-        function_snapshot_path="./snapshots/logs/$function_name.log"
-
+    for parameters_set in "${PARAMETERS_SETS[@]}"; do
+        function_name="${handler_name}_python"
+        function_snapshot_path="./snapshots/logs/${handler_name}_${parameters_set}.log"
+        serverless_runtime=$parameters_set[0]
+        python_version=$parameters_set[1]
+        run_id=$parameters_set[2]
         # Fetch logs with serverless cli, retrying to avoid AWS account-wide rate limit error
         retry_counter=0
         while [ $retry_counter -lt 10 ]; do
-            raw_logs=$(serverless logs -f $function_name --stage $run_id --startTime $script_utc_start_time)
+            raw_logs=$(PYTHON_VERSION=${!python_version} RUNTIME=$parameters_set SERVERLESS_RUNTIME=${!serverless_runtime} \
+            serverless logs --stage ${!run_id} -f $function_name --startTime $script_utc_start_time)
             fetch_logs_exit_code=$?
             if [ $fetch_logs_exit_code -eq 1 ]; then
                 echo "Retrying fetch logs for $function_name..."
@@ -158,7 +198,7 @@ for handler_name in "${LAMBDA_HANDLERS[@]}"; do
                 sed -E "s/(dd_lambda_layer:datadog-python[0-9]+_)[0-9]+\.[0-9]+\.[0-9]+/\1X\.X\.X/g" |
                 sed -E "s/(datadog_lambda:v)([0-9]+\.[0-9]+\.[0-9])/\1XX/g" |
                 # Strip out run ID (from function name, resource, etc.)
-                sed -E "s/$run_id/XXXX/g" |
+                sed -E "s/${!run_id}/XXXX/g" |
                 # Strip out trace/span/parent/timestamps
                 sed -E "s/(\"trace_id\"\: \")[A-Z0-9\.\-]+/\1XXXX/g" |
                 sed -E "s/(\"span_id\"\: \")[A-Z0-9\.\-]+/\1XXXX/g" |
