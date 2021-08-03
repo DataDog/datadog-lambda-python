0415dfd9c1607efbb06499b90a847309c1b0143f
Dylan Yang <dylan.yang@datadoghq.com>
2021-08-03 12:05:11 -0400

    Update module version to 3.44.0

--- datadog_lambda/__init__.py
+++ datadog_lambda/__init__.py
@@ -1,6 +1,6 @@
 # The minor version corresponds to the Lambda layer version.
 # E.g.,, version 0.5.0 gets packaged into layer version 5.
-__version__ = "3.43.0"
+__version__ = "3.44.0"
 
 
 import os
