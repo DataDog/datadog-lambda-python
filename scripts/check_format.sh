5f5a213b8610d5f905797ad581c40bd53f659d9e
Harvinder <ghotra.harvinder@gmail.com>
2022-04-08 17:16:30 -0400

    Bump version for black to 22.3.0

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -2,7 +2,7 @@
 set -e
 
 PYTHON_VERSION=$(python -c 'import sys; print(sys.version_info.major)')
-pip install -Iv black==21.5b2
+pip install -Iv black==22.3.0
 
 python -m black --check datadog_lambda/ --diff
 python -m black --check tests --diff
