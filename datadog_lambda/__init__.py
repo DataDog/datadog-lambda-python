650b99dfd45ecc4ef495397f359f8866e1cd83bf
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2023-02-09 22:09:44 -0500

    bugfix

--- datadog_lambda/__init__.py
+++ datadog_lambda/__init__.py
@@ -1,20 +1,19 @@
 from datadog_lambda.cold_start import is_cold_start, wrap_find_spec
+import os
 
-if is_cold_start():
-    import os
+if (
+    is_cold_start()
+    and os.environ.get("DD_TRACE_ENABLED", "true").lower() == "true"
+    and os.environ.get("DD_COLD_START_TRACING", "true").lower() == "true"
+):
+    from sys import version_info, meta_path
 
-    if (
-        os.environ.get("DD_TRACE_ENABLED", "true").lower() == "true"
-        and os.environ.get("DD_COLD_START_TRACING", "true").lower() == "true"
-    ):
-        from sys import version_info, meta_path
-
-        if version_info >= (3, 7):  # current implementation only support version > 3.7
-            for importer in meta_path:
-                try:
-                    importer.find_spec = wrap_find_spec(importer.find_spec)
-                except:
-                    pass
+    if version_info >= (3, 7):  # current implementation only support version > 3.7
+        for importer in meta_path:
+            try:
+                importer.find_spec = wrap_find_spec(importer.find_spec)
+            except:
+                pass
 
 # The minor version corresponds to the Lambda layer version.
 # E.g.,, version 0.5.0 gets packaged into layer version 5.
