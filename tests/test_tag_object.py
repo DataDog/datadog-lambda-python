5dceccefbc6a270750e3b9b49038eb2c63864105
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2023-09-09 13:47:55 -0400

    enhancing tag_object

--- tests/test_tag_object.py
+++ tests/test_tag_object.py
@@ -19,12 +19,12 @@ class TestTagObject(unittest.TestCase):
         tag_object(spanMock, "function.request", payload)
         spanMock.set_tag.assert_has_calls(
             [
-                call("function.request.vals.0.thingOne", 1),
-                call("function.request.vals.1.thingTwo", 2),
+                call("function.request.vals.0.thingOne", "1"),
+                call("function.request.vals.1.thingTwo", "2"),
                 call("function.request.hello", "world"),
                 call("function.request.anotherThing.blah", None),
                 call("function.request.anotherThing.foo", "bar"),
-                call("function.request.anotherThing.nice", True),
+                call("function.request.anotherThing.nice", "True"),
             ],
             True,
         )
@@ -85,12 +85,57 @@ class TestTagObject(unittest.TestCase):
         )
 
     def test_decimal_tag_object(self):
-        payload = {"myValue": Decimal(500.50)}
+        payload = {"myValue": Decimal(500.5)}
         spanMock = MagicMock()
         tag_object(spanMock, "function.request", payload)
         spanMock.set_tag.assert_has_calls(
             [
-                call("function.request.myValue", Decimal(500.50)),
+                call("function.request.myValue", "500.5"),
+            ],
+            True,
+        )
+
+
+    class CustomResponse(object):
+        """
+        For example, chalice.app.Response class
+        """
+        def __init__(
+            self, body,
+            headers = None,
+            status_code: int = 200
+        ):
+            self.body = body
+            if headers is None:
+                headers = {}
+            self.headers = headers
+            self.status_code = status_code
+
+        def __str__(self):
+            return str(self.body)
+
+    class ResponseHasToDict(CustomResponse):
+        def to_dict(self):
+            return self.headers
+
+    def test_custom_response(self):
+        payload = self.CustomResponse({'hello':'world'}, {'key1':'val1'}, 200)
+        spanMock = MagicMock()
+        tag_object(spanMock, "function.response", payload)
+        spanMock.set_tag.assert_has_calls(
+            [
+                call("function.response", "{'hello': 'world'}"),
+            ],
+            True,
+        )
+
+    def test_custom_response_to_dict(self):
+        payload = self.ResponseHasToDict({'hello':'world'}, {'key1':'val1'}, 200)
+        spanMock = MagicMock()
+        tag_object(spanMock, "function.response", payload)
+        spanMock.set_tag.assert_has_calls(
+            [
+                call("function.response.key1", "val1"),
             ],
             True,
         )
