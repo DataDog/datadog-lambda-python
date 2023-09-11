080274d830f6c04fbbbde883e9be2b1f0e84c2e9
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2023-09-11 11:11:40 -0400

    enhancing tag_object (#367)
    * enhancing tag_object
    
    * format

--- datadog_lambda/tag_object.py
+++ datadog_lambda/tag_object.py
@@ -28,18 +28,27 @@ def tag_object(span, key, obj, depth=0):
             redacted = _redact_val(key, obj[0:5000])
             return span.set_tag(key, redacted)
     if isinstance(obj, int) or isinstance(obj, float) or isinstance(obj, Decimal):
-        return span.set_tag(key, obj)
+        return span.set_tag(key, str(obj))
     if isinstance(obj, list):
         for k, v in enumerate(obj):
             formatted_key = "{}.{}".format(key, k)
             tag_object(span, formatted_key, v, depth)
         return
-    if isinstance(obj, object):
-        for k in obj:
-            v = obj.get(k)
+    if hasattr(obj, "items"):
+        for k, v in obj.items():
             formatted_key = "{}.{}".format(key, k)
             tag_object(span, formatted_key, v, depth)
         return
+    if hasattr(obj, "to_dict"):
+        for k, v in obj.to_dict().items():
+            formatted_key = "{}.{}".format(key, k)
+            tag_object(span, formatted_key, v, depth)
+        return
+    try:
+        value_as_str = str(obj)
+    except Exception:
+        value_as_str = "UNKNOWN"
+    return span.set_tag(key, value_as_str)
 
 
 def _should_try_string(obj):
