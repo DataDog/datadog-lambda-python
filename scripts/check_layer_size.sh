a1c1a11572adc99a3cfe9c940fc53f29a17f74d1
AJ Stuyvenberg <astuyve@gmail.com>
2024-03-11 21:37:31 -0400

    feat: 13m?

--- scripts/check_layer_size.sh
+++ scripts/check_layer_size.sh
@@ -10,7 +10,7 @@
 # 7 mb size limit
 set -e
 MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 5 \* 1024)
-MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 10 \* 1024)
+MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 13 \* 1024)
 
 
 LAYER_FILES_PREFIX="datadog_lambda_py"
