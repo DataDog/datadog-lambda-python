aad3cd802e37f3dbc93a2356f4711b519788a332
Abhinav Vedmala <abhinav.vedmala@datadoghq.com>
2024-11-04 17:06:42 -0500

    lint

--- tests/test_xray.py
+++ tests/test_xray.py
@@ -34,9 +34,9 @@ class TestXRay(unittest.TestCase):
 
     def test_send_segment_sampled_out(self):
         os.environ["AWS_XRAY_DAEMON_ADDRESS"] = "fake-agent.com:8080"
-        os.environ["_X_AMZN_TRACE_ID"] = (
-            "Root=1-5e272390-8c398be037738dc042009320;Parent=94ae789b969f1cc5;Sampled=0;Lineage=c6c5b1b9:0"
-        )
+        os.environ[
+            "_X_AMZN_TRACE_ID"
+        ] = "Root=1-5e272390-8c398be037738dc042009320;Parent=94ae789b969f1cc5;Sampled=0;Lineage=c6c5b1b9:0"
 
         with patch(
             "datadog_lambda.xray.sock.send", MagicMock(return_value=None)
@@ -47,9 +47,9 @@ class TestXRay(unittest.TestCase):
 
     def test_send_segment_sampled(self):
         os.environ["AWS_XRAY_DAEMON_ADDRESS"] = "fake-agent.com:8080"
-        os.environ["_X_AMZN_TRACE_ID"] = (
-            "Root=1-5e272390-8c398be037738dc042009320;Parent=94ae789b969f1cc5;Sampled=1;Lineage=c6c5b1b9:0"
-        )
+        os.environ[
+            "_X_AMZN_TRACE_ID"
+        ] = "Root=1-5e272390-8c398be037738dc042009320;Parent=94ae789b969f1cc5;Sampled=1;Lineage=c6c5b1b9:0"
         with patch(
             "datadog_lambda.xray.sock.send", MagicMock(return_value=None)
         ) as mock_send:
