ca7e5c51fcbccd6b7a39d24388e0c7bb31832ccf
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-03-11 16:51:31 -0400

    lint

--- datadog_lambda/api.py
+++ datadog_lambda/api.py
@@ -71,8 +71,7 @@ def get_api_key() -> str:
         # Secrets manager endpoints: https://docs.aws.amazon.com/general/latest/gr/asm.html
         fips_endpoint = f"https://secretsmanager-fips.{REGION}.amazonaws.com" if is_gov_region else None
         secrets_manager_client = boto3.client(
-            "secretsmanager",
-            endpoint_url=fips_endpoint
+            "secretsmanager", endpoint_url=fips_endpoint
         )
         api_key = secrets_manager_client.get_secret_value(
             SecretId=DD_API_KEY_SECRET_ARN
@@ -80,20 +79,14 @@ def get_api_key() -> str:
     elif DD_API_KEY_SSM_NAME:
         # SSM endpoints: https://docs.aws.amazon.com/general/latest/gr/ssm.html
         fips_endpoint = f"https://ssm-fips.{REGION}.amazonaws.com" if is_gov_region else None
-        ssm_client = boto3.client(
-            "ssm",
-            endpoint_url=fips_endpoint
-        )
+        ssm_client = boto3.client("ssm", endpoint_url=fips_endpoint)
         api_key = ssm_client.get_parameter(
             Name=DD_API_KEY_SSM_NAME, WithDecryption=True
         )["Parameter"]["Value"]
     elif DD_KMS_API_KEY:
         # KMS endpoints: https://docs.aws.amazon.com/general/latest/gr/kms.html
         fips_endpoint = f"https://kms-fips.{REGION}.amazonaws.com" if is_gov_region else None
-        kms_client = boto3.client(
-            "kms",
-            endpoint_url=fips_endpoint
-        )
+        kms_client = boto3.client("kms",endpoint_url=fips_endpoint)
         api_key = decrypt_kms_api_key(kms_client, DD_KMS_API_KEY)
     else:
         api_key = DD_API_KEY
