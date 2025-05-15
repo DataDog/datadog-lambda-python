d2257986bf276fc9fdd6fa2c5f224d02bcd4e3f1
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-05-15 14:00:14 -0700

    Tests for reading config values.

--- datadog_lambda/config.py
+++ datadog_lambda/config.py
@@ -9,15 +9,21 @@ import os
 
 def _get_env(key, default=None, cast=None):
     """Get an environment variable with a default value."""
-    val = os.environ.get(key, default)
-    if cast is not None:
-        try:
-            val = cast(val)
-        except ValueError:
-            raise ValueError(f"Invalid value for {key}: {val}")
-            return cast(default)
-    return val
+    prop_key = f"_{key}"
 
+    @property
+    def _getter(self):
+        if not hasattr(self, prop_key):
+            val = os.environ.get(key, default)
+            if cast is not None:
+                try:
+                    val = cast(val)
+                except ValueError:
+                    raise ValueError(f"Invalid value for {key}: {val}")
+                    return cast(default)
+        return getattr(self, prop_key)
+
+    return _getter
 
 def as_bool(val):
     """Convert a string to a boolean."""
@@ -32,38 +38,40 @@ def as_bool(val):
     raise ValueError(f"Invalid boolean value: {val}")
 
 
-class config:
+class Config:
 
-    function_name = os.environ.get("AWS_LAMBDA_FUNCTION_NAME")
-    flush_to_log = os.environ.get("DD_FLUSH_TO_LOG", "").lower() == "true"
-    trace_enabled = os.environ.get("DD_TRACE_ENABLED", "true").lower() == "true"
-    cold_start_tracing = (
-        os.environ.get("DD_COLD_START_TRACING", "true").lower() == "true"
-    )
-    is_gov_region = os.environ.get("AWS_REGION", "").startswith("us-gov-")
-    fips_mode_enabled = (
-        os.environ.get(
-            "DD_LAMBDA_FIPS_MODE",
-            "true" if is_gov_region else "false",
-        ).lower()
-        == "true"
-    )
-    log_level = (os.environ.get("DD_LOG_LEVEL") or "INFO").upper()
-    flush_in_thread = os.environ.get("DD_FLUSH_IN_THREAD", "").lower() == "true"
-    enhanced_metrics_enabled = (
-        os.environ.get("DD_ENHANCED_METRICS", "true").lower() == "true"
-    )
-    is_in_tests = os.environ.get("DD_INTEGRATION_TEST", "false").lower() == "true"
-    add_span_pointers = os.environ.get(
-        "DD_BOTOCORE_ADD_SPAN_POINTERS", "true"
-    ).lower() in ("true", "1")
-    otel_enabled = os.environ.get("DD_TRACE_OTEL_ENABLED", "false").lower() == "true"
-    is_lambda_context = bool(function_name)
-    telemetry_enabled = (
-        os.environ.get("DD_INSTRUMENTATION_TELEMETRY_ENABLED", "false").lower()
-        == "true"
-    )
+    def __init__(self):
+        self.function_name = os.environ.get("AWS_LAMBDA_FUNCTION_NAME")
+        self.flush_to_log = os.environ.get("DD_FLUSH_TO_LOG", "").lower() == "true"
+        self.trace_enabled = os.environ.get("DD_TRACE_ENABLED", "true").lower() == "true"
+        self.cold_start_tracing = (
+            os.environ.get("DD_COLD_START_TRACING", "true").lower() == "true"
+        )
+        self.is_gov_region = os.environ.get("AWS_REGION", "").startswith("us-gov-")
+        self.fips_mode_enabled = (
+            os.environ.get(
+                "DD_LAMBDA_FIPS_MODE",
+                "true" if self.is_gov_region else "false",
+            ).lower()
+            == "true"
+        )
+        self.flush_in_thread = os.environ.get("DD_FLUSH_IN_THREAD", "").lower() == "true"
+        self.enhanced_metrics_enabled = (
+            os.environ.get("DD_ENHANCED_METRICS", "true").lower() == "true"
+        )
+        self.is_in_tests = os.environ.get("DD_INTEGRATION_TEST", "false").lower() == "true"
+        self.add_span_pointers = os.environ.get(
+            "DD_BOTOCORE_ADD_SPAN_POINTERS", "true"
+        ).lower() in ("true", "1")
+        self.otel_enabled = os.environ.get("DD_TRACE_OTEL_ENABLED", "false").lower() == "true"
+        self.is_lambda_context = bool(self.function_name)
+        self.telemetry_enabled = (
+            os.environ.get("DD_INSTRUMENTATION_TELEMETRY_ENABLED", "false").lower()
+            == "true"
+        )
 
+
+config = Config()
 
 if config.is_gov_region or config.fips_mode_enabled:
     logger = logging.getLogger(__name__)
