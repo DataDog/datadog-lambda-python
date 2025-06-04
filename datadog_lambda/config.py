1df2eb742f5ce2448bf85bff4ba4d3aa77e4b3cc
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-06-04 14:05:22 -0700

    reorder.

--- datadog_lambda/config.py
+++ datadog_lambda/config.py
@@ -49,6 +49,10 @@ class Config:
     service = _get_env("DD_SERVICE")
     env = _get_env("DD_ENV")
 
+    function_name = _get_env("AWS_LAMBDA_FUNCTION_NAME", "function")
+    is_gov_region = _get_env("AWS_REGION", "", lambda x: x.startswith("us-gov-"))
+    is_lambda_context = _get_env("AWS_LAMBDA_FUNCTION_NAME", None, bool)
+
     cold_start_tracing = _get_env(
         "DD_COLD_START_TRACING", "true", as_bool, depends_on_tracing=True
     )
@@ -62,26 +66,7 @@ class Config:
     capture_payload_max_depth = _get_env("DD_CAPTURE_LAMBDA_PAYLOAD_MAX_DEPTH", 10, int)
     capture_payload_enabled = _get_env("DD_CAPTURE_LAMBDA_PAYLOAD", "false", as_bool)
 
-    add_span_pointers = _get_env("DD_BOTOCORE_ADD_SPAN_POINTERS", "true", as_bool)
-    enhanced_metrics_enabled = _get_env("DD_ENHANCED_METRICS", "true", as_bool)
-    flush_in_thread = _get_env("DD_FLUSH_IN_THREAD", "false", as_bool)
-    flush_to_log = _get_env("DD_FLUSH_TO_LOG", "false", as_bool)
-    logs_injection = _get_env("DD_LOGS_INJECTION", "true", as_bool)
-    function_name = _get_env("AWS_LAMBDA_FUNCTION_NAME", "function")
-    is_gov_region = _get_env("AWS_REGION", "", lambda x: x.startswith("us-gov-"))
-    integration_test = _get_env("DD_INTEGRATION_TEST", "false", as_bool)
-    is_lambda_context = _get_env("AWS_LAMBDA_FUNCTION_NAME", None, bool)
-    otel_enabled = _get_env("DD_TRACE_OTEL_ENABLED", "false", as_bool)
-    telemetry_enabled = _get_env(
-        "DD_INSTRUMENTATION_TELEMETRY_ENABLED", "false", as_bool
-    )
     trace_enabled = _get_env("DD_TRACE_ENABLED", "true", as_bool)
-    merge_xray_traces = _get_env("DD_MERGE_XRAY_TRACES", "false", as_bool)
-    trace_extractor = _get_env("DD_TRACE_EXTRACTOR")
-    profiling_enabled = _get_env("DD_PROFILING_ENABLED", "false", as_bool)
-    llmobs_enabled = _get_env("DD_LLMOBS_ENABLED", "false", as_bool)
-    exception_replay_enabled = _get_env("DD_EXCEPTION_REPLAY_ENABLED", "false", as_bool)
-
     make_inferred_span = _get_env(
         "DD_TRACE_MANAGED_SERVICES", "true", as_bool, depends_on_tracing=True
     )
@@ -91,8 +76,26 @@ class Config:
     decode_authorizer_context = _get_env(
         "DD_DECODE_AUTHORIZER_CONTEXT", "true", as_bool, depends_on_tracing=True
     )
+    add_span_pointers = _get_env("DD_BOTOCORE_ADD_SPAN_POINTERS", "true", as_bool)
+    trace_extractor = _get_env("DD_TRACE_EXTRACTOR")
+
+    enhanced_metrics_enabled = _get_env("DD_ENHANCED_METRICS", "true", as_bool)
+
+    flush_in_thread = _get_env("DD_FLUSH_IN_THREAD", "false", as_bool)
+    flush_to_log = _get_env("DD_FLUSH_TO_LOG", "false", as_bool)
+    logs_injection = _get_env("DD_LOGS_INJECTION", "true", as_bool)
+    merge_xray_traces = _get_env("DD_MERGE_XRAY_TRACES", "false", as_bool)
+
+    telemetry_enabled = _get_env(
+        "DD_INSTRUMENTATION_TELEMETRY_ENABLED", "false", as_bool
+    )
+    otel_enabled = _get_env("DD_TRACE_OTEL_ENABLED", "false", as_bool)
+    profiling_enabled = _get_env("DD_PROFILING_ENABLED", "false", as_bool)
+    llmobs_enabled = _get_env("DD_LLMOBS_ENABLED", "false", as_bool)
+    exception_replay_enabled = _get_env("DD_EXCEPTION_REPLAY_ENABLED", "false", as_bool)
 
     local_test = _get_env("DD_LOCAL_TEST", "false", as_bool)
+    integration_test = _get_env("DD_INTEGRATION_TEST", "false", as_bool)
 
     @property
     def fips_mode_enabled(self):
