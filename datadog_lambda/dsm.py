54bedbf83a2596a6e2bb456dbe3d48b747ed8d18
Michael Zhao <michael.zhao@datadoghq.com>
2025-06-10 15:23:03 -0400

    fixes

--- datadog_lambda/dsm.py
+++ datadog_lambda/dsm.py
@@ -1,10 +1,9 @@
 import json
+import logging
 
-from ddtrace.internal.logger import get_logger
-from datadog_lambda import logger
 from datadog_lambda.trigger import EventTypes
 
-log = get_logger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def set_dsm_context(event, event_source):
@@ -22,7 +21,6 @@ def _dsm_set_context_helper(service_type, arn, payload_size, context_json):
         payload_size: payload size of the record
         context_json: Datadog context for the record
     """
-    from datadog_lambda.wrapper import format_err_with_traceback
     from ddtrace.internal.datastreams import data_streams_processor
     from ddtrace.internal.datastreams.processor import DsmPathwayCodec
 
@@ -35,7 +33,7 @@ def _dsm_set_context_helper(service_type, arn, payload_size, context_json):
             payload_size=payload_size,
         )
     except Exception as e:
-        logger.error(format_err_with_traceback(e))
+        logger.error(f"Unable to set dsm context: {e}")
 
 
 def _dsm_set_sqs_context(event):
@@ -61,11 +59,11 @@ def _get_dsm_context_from_lambda(message):
     context_json = None
     message_attributes = message.get("messageAttributes")
     if not message_attributes:
-        log.debug("DataStreams skipped lambda message: %r", message)
+        logger.debug("DataStreams skipped lambda message: %r", message)
         return None
 
     if "_datadog" not in message_attributes:
-        log.debug("DataStreams skipped lambda message: %r", message)
+        logger.debug("DataStreams skipped lambda message: %r", message)
         return None
 
     datadog_attr = message_attributes["_datadog"]
@@ -74,6 +72,6 @@ def _get_dsm_context_from_lambda(message):
         # SQS -> lambda
         context_json = json.loads(datadog_attr["stringValue"])
     else:
-        log.debug("DataStreams did not handle lambda message: %r", message)
+        logger.debug("DataStreams did not handle lambda message: %r", message)
 
     return context_json
