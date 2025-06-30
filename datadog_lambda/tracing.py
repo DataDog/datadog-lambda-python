c84ea1442c4500a3bc8e3631cde3f721c114954c
Michael Zhao <michael.zhao@datadoghq.com>
2025-06-30 19:10:15 -0400

    fixes

--- datadog_lambda/tracing.py
+++ datadog_lambda/tracing.py
@@ -68,8 +68,6 @@ LOWER_64_BITS = "LOWER_64_BITS"
 
 
 def _dsm_set_checkpoint(context_json, event_type, arn):
-    from ddtrace.data_streams import set_consume_checkpoint
-
     if not isinstance(context_json, dict):
         return
 
@@ -77,19 +75,14 @@ def _dsm_set_checkpoint(context_json, event_type, arn):
         return
 
     try:
-        carrier_get = _create_carrier_get(context_json)
+        from ddtrace.data_streams import set_consume_checkpoint
+
+        carrier_get = lambda k: context_json.get(k)
         set_consume_checkpoint(event_type, arn, carrier_get, manual_checkpoint=False)
     except Exception as e:
         logger.debug(
             f"DSM:Failed to set consume checkpoint for {event_type} {arn}: {e}"
         )
-
-
-def _create_carrier_get(context_json):
-    def carrier_get(key):
-        return context_json.get(key)
-
-    return carrier_get
 
 
 def _convert_xray_trace_id(xray_trace_id):
