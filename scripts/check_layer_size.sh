f5b8a4e9f676a8d90cc657f3c7a9585b92804b17
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-07-24 12:13:50 -0400

    bump up layer size limit

--- scripts/check_layer_size.sh
+++ scripts/check_layer_size.sh
@@ -8,8 +8,8 @@
 # Compares layer size to threshold, and fails if below that threshold
 
 set -e
-MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 6 \* 1024)
-MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 15 \* 1024)
+MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 8 \* 1024)
+MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 22 \* 1024)
 
 
 LAYER_FILES_PREFIX="datadog_lambda_py"
