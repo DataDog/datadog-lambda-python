5a3ad86e61b146e0e3201e679281c07b17323b73
Rithika Narayan <rithika.narayan@datadoghq.com>
2025-09-29 18:56:42 -0400

    Bump zipped layer size

--- scripts/check_layer_size.sh
+++ scripts/check_layer_size.sh
@@ -8,7 +8,7 @@
 # Compares layer size to threshold, and fails if below that threshold
 
 set -e
-MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 17 \* 1024 / 2)  # 8704 KB
+MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 17.5 \* 1024 / 2)  # 8960 KB
 MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 25 \* 1024)   # 25600 KB
 
 
