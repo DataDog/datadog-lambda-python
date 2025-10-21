d0c354ed57797e0144f33c422a72763d2f76a83b
Rithika Narayan <rithika.narayan@datadoghq.com>
2025-10-21 16:24:44 -0400

    strip unneeded

--- pub_layer.sh
+++ pub_layer.sh
@@ -1,8 +1,8 @@
 layer_arn=$(
 aws-vault exec sso-serverless-sandbox-account-admin -- \
-	aws lambda publish-layer-version --layer-name "Python312-discard-all-rithika" \
+	aws lambda publish-layer-version --layer-name "Python312-strip-debug-rithika" \
 		--description "Datadog Tracer Lambda Layer for Python" \
-		--zip-file "fileb://./discard-all.zip" \
+		--zip-file "fileb://./.layers/datadog_lambda_py-arm64-3.12.zip" \
 		--region "us-west-2" \
 		--output json \
 			| jq -r '.LayerVersionArn')
