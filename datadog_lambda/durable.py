04c6ddd96480bc09287006eb9c27aad6c2689a75
Yiming Luo <yiming.luo@datadoghq.com>
2026-02-19 16:19:57 -0500

    Change log level from debug to error

--- datadog_lambda/durable.py
+++ datadog_lambda/durable.py
@@ -38,7 +38,7 @@ def extract_durable_function_tags(event):
 
     parsed = _parse_durable_execution_arn(durable_execution_arn)
     if not parsed:
-        logger.debug(
+        logger.error(
             "Failed to parse DurableExecutionArn: %s", durable_execution_arn
         )
         return {}
